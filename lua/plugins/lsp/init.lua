1f02d202cffdc62754e98b89472976d8cf8a3033
ShangYJQ <421207553@qq.com>
2025-02-22 06:39:07 +0000

    dev

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -23,8 +23,8 @@ return {
         ['Down'] = { 'scroll_documentation_down', 'fallback' },
         ['Up'] = { 'scroll_documentation_up', 'fallback' },
 
-        ['<Tab>'] = { 'snippet_forward', 'fallback' },
-        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
 
         ['<Up>'] = { 'select_prev', 'fallback' },
         ['<Down>'] = { 'select_next', 'fallback' },
