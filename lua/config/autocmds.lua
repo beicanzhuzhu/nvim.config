4e29c9451877d88925abc1cf15dbe97c49e4a8a3
ShangYJQ <421207553@qq.com>
2025-03-06 16:24:46 +0800

    add .h auto save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,7 @@ local autosavefiletype = {
   "*.cpp",
   "*.c",
   "*.py",
+  "*.h",
 }
 
 vim.api.nvim_create_autocmd('VimEnter',
@@ -32,14 +33,14 @@ vim.api.nvim_create_autocmd("CmdlineEnter", {
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    -- group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client.server_capabilities.inlayHintProvider then
-            vim.lsp.inlay_hint.enable()
-        end
-        -- whatever other lsp config you want
+  -- group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable()
     end
+    -- whatever other lsp config you want
+  end
 })
 
 -- Fix conceallevel for json files
