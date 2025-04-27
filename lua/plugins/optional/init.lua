0e6cf556da6672f66ca519826b574c7135e80067
ShangYJQ <421207553@qq.com>
2025-04-28 01:47:01 +0800

    fix

--- lua/plugins/optional/init.lua
+++ lua/plugins/optional/init.lua
@@ -1,5 +1,5 @@
 -- these plugins is options, your can turn on and turn off these plugins there
 return {
-    require("plugins.options.noice"),
-    require("plugins.options.smear-cursor")
+    require("plugins.optional.noice"),
+    require("plugins.optional.smear-cursor")
 }
