c90dbc6cff0f9da8614f7f27469fcbfe0abdd60e
ShangYJQ <421207553@qq.com>
2025-09-10 15:52:23 +0800

    remove:llm

--- init.lua
+++ init.lua
@@ -7,11 +7,6 @@ if vim.uv.os_uname().sysname == "Windows_NT" then
     vim.g.py_exec = "python"
 end
 
--- set for AI
--- Use openAI model
--- You need to set LLM_KEY and DEEPSEEK_KEY
-vim.g.AI_URL = "https://apic.littlewheat.com/v1/chat/completions"
-
 -- load lazy.nvim plugins mamager
 require("config.lazy")
 
--- lua/plugins/optional/init.lua
+++ lua/plugins/optional/init.lua
@@ -14,12 +14,4 @@ else
     vim.notify("Running in Neovide, disable smear-cursor.")
 end
 
--- if set LLM_LEY
-if vim.env.LLM_KEY ~= "" and vim.env.LLM_KEY ~= nil then
-    print("Enable llm.nvim")
-    table.insert(optional_plugins, require("plugins.optional.llm"))
-else
-    vim.notify("You can set LLM_KEY to use llm.nvim plugin")
-end
-
 return optional_plugins
